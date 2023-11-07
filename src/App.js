ca337812b143d4aae8a44fcb79f7deb94c27bc32
Adam Dougal <adamdougal@microsoft.com>
2023-11-07 15:03:30 +0000

    Add app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import React from 'react';
+// add an import for the analyzeImage function
+import { analyzeImage } from './azure-image-analysis';
 
+// Create a simple gui with:
+// A title of "Computer Vision"
+// A text box to enter the URL of the image to be analyzed or the prompt of the image to generate with a label
+// A button to trigger the image analysis and one to trigger image generation
+// Ensure to hold the UI and to have a processing indicator while the analysis is running
+// Display the results using the DisplayResults function
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+    const [imageUrl, setImageUrl] = React.useState("");
+    const [imageDescription, setImageDescription] = React.useState("");
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const handleImageUrlChange = (event) => {
+        setImageUrl(event.target.value);
+    };
+
+    const handleAnalyzeImage = () => {
+        setIsLoading(true);
+        analyzeImage(imageUrl)
+            .then((description) => {
+                setImageDescription(description);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
+
+    return (
+        <div>
+            <h1>Computer Vision</h1>
+            <label>Image URL:</label>
+            <input type="text" value={imageUrl} onChange={handleImageUrlChange} />
+            <button onClick={handleAnalyzeImage}>Analyze Image</button>
+            {isLoading && <p>Loading...</p>}
+            {imageDescription && <DisplayResults imageUrl={imageUrl} imageDescription={imageDescription} />}
+        </div>
+    );
+}
+
+// add a 'DisplayResults' function to display the results of the API call in a readable format, along with the URL of the processed image
+function DisplayResults(props) {
+    return (
+        <div>
+            <h2>Results</h2>
+            <p>Image URL: {props.imageUrl}</p>
+            <p>Image description: {props.imageDescription}</p>
+            <p><img alt={props.imageDescription} src={props.imageUrl}/></p>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/azure-image-analysis.js
@@ -0,0 +1,28 @@
+// a new function called analyzeImage that calls the Azure AI Vision service Image Analysis 4.0 API as documented here https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/how-to/call-analyze-image-40?pivots=programming-language-rest-api
+// the endpoint is https://analyze-and-generate-images-dougal.cognitiveservices.azure.com/
+// the key is afc25871ed7940af894ee1441756594e
+// function should receive as input the image URL and return the JSON response of the API
+import React from 'react';
+
+function analyzeImage(imageUrl) { 
+    const endpoint = "https://analyze-and-generate-images-dougal.cognitiveservices.azure.com/";
+    
+    const key = process.env.REACT_APP_SUB_KEY;
+    
+    const headers = {
+        "Ocp-Apim-Subscription-Key": key,
+        "content-type": "application/json"
+    };
+    const body = JSON.stringify({
+        "url": imageUrl
+    });
+    return fetch(`${endpoint}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=caption`, {
+        method: 'POST',
+        headers,
+        body
+    })
+        .then(response => response.json())
+        .then(response => response.captionResult.text)
+}
+// export funciton to be used in App.js
+export { analyzeImage };
